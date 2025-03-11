1be68ac499ca397a0ae131f737423c2946fa2847
Stretsh <stretsh@gmail.com>
2025-03-11 22:12:47 +0100

    Improves animations

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -9,22 +9,42 @@ const props = defineProps(['task'])
 </script>
 
 <template>
-  <div class="flex justify-between items-center p-3 bg-white dark:bg-gray-800 dark:text-white shadow-md rounded-md transition-all">
+  <div
+    class="flex justify-between items-center p-3 bg-white dark:bg-gray-800 dark:text-white shadow-md rounded-md transition-all">
     <div class="flex-1">
       <template v-if="store.editingTaskId === task.id">
-        <TaskInput :task="task"/>
+        <transition name="fade">
+          <TaskInput :task="task"/>
+        </transition>
       </template>
 
-      <template v-else>
-        <span class="text-sm font-semibold text-gray-500">{{ task.category }}</span>
-        <p :class="{ 'line-through': task.completed }" class="text-lg" @dblclick="store.setEditingTask(task.id)">{{ task.text }}</p>
-        <p v-if="task.dueDate" class="text-xs text-gray-500">Due: {{ task.dueDate }}</p>
-      </template>
+      <transition name="fade">
+        <div v-if="store.editingTaskId !== task.id">
+          <span class="text-sm font-semibold text-gray-500">{{ task.category }}</span>
+          <p :class="{ 'line-through': task.completed }" class="text-lg" @dblclick="store.setEditingTask(task.id)">
+            {{ task.text }}</p>
+          <p v-if="task.dueDate" class="text-xs text-gray-500">Due: {{ task.dueDate }}</p>
+        </div>
+      </transition>
     </div>
 
     <div v-if="!store.editingTaskId" class="flex gap-2">
-      <button @click="store.toggleTask(task.id)" class="text-green-500 hover:text-green-700 transition"><IconCheckmark/></button>
-      <button @click="store.deleteTask(task.id)" class="text-red-500 hover:text-red-700 transition"><IconCancel /></button>
+      <button @click="store.toggleTask(task.id)" class="text-green-500 hover:text-green-700 transition">
+        <IconCheckmark/>
+      </button>
+      <button @click="store.deleteTask(task.id)" class="text-red-500 hover:text-red-700 transition">
+        <IconCancel/>
+      </button>
     </div>
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+</style>
